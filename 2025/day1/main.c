f28d0a53dd1569db9bb60e1212b645339d345ac2
Perry Vorselaars <p.vorselaars@gmail.com>
2025-12-01 21:00:04 +0100

    Day 1, clean up

--- 2025/day1/main.c
+++ 2025/day1/main.c
@@ -5,46 +5,31 @@ int main(int argc, char *argv[])
 {
     FILE *input = fopen("input", "r");
 
-    char *buf = NULL;
-    size_t len = 0;
-    ssize_t n;
+    char buf[256];
 
     int dial = 50;
     int rot = 0;
     int count = 0;
+    int wraps = 0;
 
-    while ((n = getline(&buf, &len, input)) != -1)
+    while (fgets(buf, 256, input) != NULL)
     {
         int prev = dial;
         rot = atoi(&buf[1]);
-        printf("dial: %d ", dial);
-        printf("rotate: %d ", rot);
 
-        count += rot / 100;
+        wraps += rot / 100;
         rot %= 100;
 
-        if (buf[0] == 'L')
-        {
-            printf("L ");
-            dial -= rot;
-        }
-
-        if (buf[0] == 'R')
-        {
-            printf("R ");
-            dial += rot;
-        }
+        dial += buf[0] == 'L' ? -rot : rot;
 
         if (prev != 0 && dial < 0 || dial >= 101 )
-            count++;
+            wraps++;
 
         dial = ((dial % 100) + 100) % 100;
 
         if (dial == 0)
             count++;
-
-        printf("dial: %d\n", dial);
     }
 
-    printf("count: %d\n", count);
+    printf("password 1: %d\npassword 2: %d\n", count, count + wraps);
 }
