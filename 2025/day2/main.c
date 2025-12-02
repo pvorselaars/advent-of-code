19dc9751aa67bb8ba85161010eb26f1d1ad80414
Perry Vorselaars <p.vorselaars@gmail.com>
2025-12-02 19:21:45 +0100

    Day 2, precompute for speed

--- 2025/day2/main.c
+++ 2025/day2/main.c
@@ -2,12 +2,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <math.h>
 
-int digits(unsigned long number)
+#define MAX_DIGITS 20
+
+
+unsigned long int_pow10(int exp)
 {
-    if (number == 0) return 1;
-    return (long)log10(number) +1;
+    unsigned long result = 1;
+    while (exp--)
+        result *= 10;
+
+    return result;
+}
+
+int digits(unsigned long x)
+{
+    int d = 1;
+    while (x >= 10)
+    {
+        x /= 10;
+        d++;
+    }
+    return d;
+}
+
+unsigned long r_table[MAX_DIGITS][MAX_DIGITS];
+
+void precompute_r()
+{
+    for (int d = 1; d <= MAX_DIGITS; d++)
+    {
+        for (int k = 2; k <= d; k++)
+        {
+            int m = d / k;
+            if (m == 0) continue;
+            r_table[d][k] = (int_pow10(m * k) - 1) / (int_pow10(m) - 1);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -19,20 +50,21 @@ int main(int argc, char *argv[])
     ssize_t n;
 
     unsigned long sum = 0;
+    precompute_r();
 
     while ((n = getdelim(&buf, &len, ',', input)) != -1)
     {
-        char *start = strtok(buf, "-");
-        char *end = strtok(NULL, ",");
+        unsigned long start = atol(strtok(buf, "-"));
+        unsigned long end = atol(strtok(NULL, ","));
 
-        for (unsigned long i = atol(start); i <= atol(end); i++)
+        for (unsigned long i = start; i <= end; i++)
         {
             int d = digits(i);
 
             for (int k = 2; k <= d; k++) 
             {
                 int m = d / k;
-                int r = (pow(10, m * k) - 1) / (pow(10, m) - 1);
+                int r = r_table[d][k];
 
                 if (i % r == 0 && digits(i/r) == m)
                 {
