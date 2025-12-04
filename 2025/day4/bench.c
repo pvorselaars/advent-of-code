2060efc80798a04f37a1b1eda7c705df2d33ac36
Perry Vorselaars <p.vorselaars@gmail.com>
2025-12-04 17:31:34 +0100

    Retab

--- 2025/day4/bench.c
+++ 2025/day4/bench.c
@@ -6,21 +6,21 @@
 int accessible(char *grid, uint64_t size, uint32_t x, uint32_t y)
 {
     if (grid[y*size+x] != '@')
-	return 0;
+    	return 0;
 
     uint32_t c = 0;
 
     for (int i = -1; i <= 1; i++)
     {
-    	for (int j = -1; j <= 1; j++)
-	{
-	    if (x + i < 0 || x + i >= size || y + j < 0 || y + j >= size || (i == 0 && j == 0))
-	        continue;
+        for (int j = -1; j <= 1; j++)
+        {
+            if (x + i < 0 || x + i >= size || y + j < 0 || y + j >= size || (i == 0 && j == 0))
+                continue;
 
-	    if(grid[(y+j)*size+(x+i)] == '@')
-		c++;
+            if(grid[(y+j)*size+(x+i)] == '@')
+                c++;
 
-	}
+        }
     }
 
     return c < 4 ? 1 : 0;
@@ -32,14 +32,14 @@ int remove_rolls(char *grid, uint64_t width, uint64_t *answer)
 
     for (int y = 0; y < width; y++)
     {
-	for (int x = 0; x < width; x++)
-	{
-	    if(accessible(grid, width, x, y))
-	    {
-		grid[y*width+x] = '.';
-		removed++;
-	    }
-	}
+        for (int x = 0; x < width; x++)
+        {
+            if(accessible(grid, width, x, y))
+            {
+                grid[y*width+x] = '.';
+                removed++;
+            }
+        }
     }
 
     *answer += removed;
@@ -60,7 +60,7 @@ uint64_t solution2(FILE *input)
     rewind(input);
 
     while ((n = getline(&buf, &len, input)) != -1)
-	buf += n - 1;
+    	buf += n - 1;
 
     uint64_t answer = 0;
     while(remove_rolls(grid, width, &answer));
